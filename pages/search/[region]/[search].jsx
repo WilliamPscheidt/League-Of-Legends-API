33bbe413a35f0874ab6b03482ab2cbf6609b2ba7
William Pscheidt <williampscheidt@hotmail.com>
2022-08-15 23:49:40 -0300

    Atualization

--- pages/search/[region]/[search].jsx
+++ pages/search/[region]/[search].jsx
@@ -40,8 +40,56 @@ const search = (props) => {
         return data.puuid === props.playerInformations.puuid
     }
 
-    function TeamFilter(data) {
-        return data.teamId === "100"
+    function TeamBlueFilter(data) {
+        return data.teamId == "100"
+    }
+
+    function TeamRedFilter(data) {
+        return data.teamId == "200"
+    }
+
+    function SpellFilter(data, number) {
+        let DataFilter;
+
+        switch (number) {
+            case 1:
+                DataFilter = data.summoner1Id
+                break;
+            case 2:
+                DataFilter = data.summoner2Id
+                break;
+        
+            default:
+                break;
+        }
+
+        switch (DataFilter) {
+            case 21:
+                return <img src="/cdn/spells/Barrier.webp" className='speel'/>
+            case 1:
+                return <img src="/cdn/spells/Cleanse.webp" className='speel'/>
+            case 14:
+                return <img src="/cdn/spells/Ignite.webp" className='speel'/>
+            case 3:
+                return <img src="/cdn/spells/Exhaust.webp" className='speel'/>
+            case 4:
+                return <img src="/cdn/spells/Flash.webp" className='speel'/>
+            case 6:
+                return <img src="/cdn/spells/Ghost.webp" className='speel'/>
+            case 7:
+                return <img src="/cdn/spells/Heal.webp" className='speel'/>
+            case 13:
+                return <img src="/cdn/spells/Clarity.webp" className='speel'/>
+            case 31:
+                return <img src="/cdn/spells/Mark.webp" className='speel'/>
+            case 11:
+                return <img src="/cdn/spells/Smite.webp" className='speel'/>
+            case 12:
+                return <img src="/cdn/spells/Teleport.webp" className='speel'/>
+            default:
+                console.log("Placeholder")
+                break;
+        }
     }
 
     if (props.playerInformations && props.playerQueueInformations) {
@@ -101,29 +149,67 @@ const search = (props) => {
                                     {
                                         props.playerMatchInfos.map((i) => {
                                             const PlayerInfo = i.info.participants.filter(DataFilter)
+                                            console.log(PlayerInfo)
+                                            const TeamBlue = i.info.participants.filter(TeamBlueFilter)
+                                            const TeamRed = i.info.participants.filter(TeamRedFilter)
                                             return [
                                                 <div key={i} className='match_history' id={PlayerInfo[0].win == true ? "victory" : "defeat"}>
                                                     {
                                                         i.info.gameCreation
                                                             ?
                                                             <>
-                                                                <div>
+                                                                <div className='container_infos'>
+                                                                    <div className='container_profile_image'>
+                                                                        {PlayerInfo[0] && <img src={"/cdn/champion/" + PlayerInfo[0].championName + ".png"} className="champ_image"/>}
+                                                                        <span className='champ_level'>{PlayerInfo[0].champLevel}</span>
+                                                                    </div>
 
+                                                                    <div className='container_infos_internal'>
+                                                                        <span id={PlayerInfo[0].win == true ? "win" : "defeated"}>{PlayerInfo[0].win == true ? "WIN" : "DEFEAT"}</span>
+                                                                        <div className='spell_container'>
+                                                                            {SpellFilter(PlayerInfo[0], 1)}
+                                                                            {SpellFilter(PlayerInfo[0], 2)}
+                                                                        </div>
+                                                                    </div>
                                                                 </div>
-                                                                <div>
-                                                                    {PlayerInfo[0].item0 > 0 ? <Image src={"/cdn/item/" + PlayerInfo[0].item0 + ".png"} alt="Player Item 1" width={50} height={50} /> : <div className='placeholder'/>}
-                                                                    {PlayerInfo[0].item1 > 0 ? <Image src={"/cdn/item/" + PlayerInfo[0].item1 + ".png"} alt="Player Item 2" width={50} height={50} /> : <div className='placeholder'/>}
-                                                                    {PlayerInfo[0].item2 > 0 ? <Image src={"/cdn/item/" + PlayerInfo[0].item2 + ".png"} alt="Player Item 3" width={50} height={50} /> : <div className='placeholder'/>}
-                                                                    {PlayerInfo[0].item3 > 0 ? <Image src={"/cdn/item/" + PlayerInfo[0].item3 + ".png"} alt="Player Item 4" width={50} height={50} /> : <div className='placeholder'/>}
-                                                                    {PlayerInfo[0].item4 > 0 ? <Image src={"/cdn/item/" + PlayerInfo[0].item4 + ".png"} alt="Player Item 5" width={50} height={50} /> : <div className='placeholder'/>}
-                                                                    {PlayerInfo[0].item5 > 0 ? <Image src={"/cdn/item/" + PlayerInfo[0].item5 + ".png"} alt="Player Item 6" width={50} height={50} /> : <div className='placeholder'/>} 
+                                                                <div className='container_kda'>
+                                                                    <div className='container_kda_internal'>
+                                                                        <span className='title'>KDA</span>
+                                                                        <span className='kda_text'>{PlayerInfo[0].kills}/{PlayerInfo[0].deaths}/{PlayerInfo[0].assists}</span>
+                                                                    </div>
+
+                                                                    <div className='container_kda_internal'>
+                                                                        <span className='title'>FARM</span>
+                                                                        <span className='kda_text'>{PlayerInfo[0].neutralMinionsKilled+PlayerInfo[0].totalMinionsKilled}</span>
+                                                                    </div>
                                                                 </div>
-                                                                <div>
-                                                                {i.info.participants.map((player) => {
-                                                                    return [
-                                                                        <Image key={player} src={"/cdn/champion/" + player.championName + ".png"} width={50} height={50} alt="Champion Image" />
-                                                                    ]
-                                                                })}
+                                                                <div className='container_items'>
+                                                                    <div>
+                                                                        {PlayerInfo[0].item0 > 0 ? <img src={"/cdn/item/" + PlayerInfo[0].item0 + ".png"} alt="Player Item 1" className='item_icon'/> : <div className='placeholder'/>}
+                                                                        {PlayerInfo[0].item1 > 0 ? <img src={"/cdn/item/" + PlayerInfo[0].item1 + ".png"} alt="Player Item 2" className='item_icon' /> : <div className='placeholder'/>}
+                                                                        {PlayerInfo[0].item2 > 0 ? <img src={"/cdn/item/" + PlayerInfo[0].item2 + ".png"} alt="Player Item 3" className='item_icon' /> : <div className='placeholder'/>}
+                                                                    </div>
+                                                                    <div>
+                                                                        {PlayerInfo[0].item3 > 0 ? <img src={"/cdn/item/" + PlayerInfo[0].item3 + ".png"} alt="Player Item 4" className='item_icon' /> : <div className='placeholder'/>}
+                                                                        {PlayerInfo[0].item4 > 0 ? <img src={"/cdn/item/" + PlayerInfo[0].item4 + ".png"} alt="Player Item 5" className='item_icon' /> : <div className='placeholder'/>}
+                                                                        {PlayerInfo[0].item5 > 0 ? <img src={"/cdn/item/" + PlayerInfo[0].item5 + ".png"} alt="Player Item 6" className='item_icon' /> : <div className='placeholder'/>}
+                                                                    </div> 
+                                                                </div>
+                                                                <div className='container_champions'>
+                                                                    <div>
+                                                                        {TeamBlue.map((player) => {
+                                                                            return [
+                                                                                <img key={player} src={"/cdn/champion/" + player.championName + ".png"} alt="Champion Image" className='champion_image'/>
+                                                                            ]
+                                                                        })}
+                                                                    </div>
+                                                                    <div>
+                                                                        {TeamRed.map((player) => {
+                                                                            return [
+                                                                                <img key={player} src={"/cdn/champion/" + player.championName + ".png"} alt="Champion Image" className='champion_image'/>
+                                                                            ]
+                                                                        })}
+                                                                    </div>
                                                                 </div>
                                                             </>
                                                             :
